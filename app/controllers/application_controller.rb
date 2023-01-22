ab69bc5c6f9f9eebc4d60ac20550fdcadb6d6dc1
broothie <adbooth8@gmail.com>
2023-01-22 12:51:10 -0800

    log error in `#authenticate_user!`

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,8 @@ class ApplicationController < ActionController::API
       authenticate_or_request_with_http_token do |token|
         jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
         @current_user_id = jwt_payload["id"]
-      rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+      rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError => error
+        logger.error error
         head :unauthorized
       end
     end
