c94bf753ab35f894d9d4c8402739b3f99f029abb
broothie <adbooth8@gmail.com>
2023-02-02 01:11:06 -0800

    fix specs

--- app/controllers/concerns/view_helpers.rb
+++ app/controllers/concerns/view_helpers.rb
@@ -17,9 +17,12 @@ module ViewHelpers
     @markdown_renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(escape_html: true), autolink: true)
   end
 
-  sig {returns(String)}
+  sig {returns(T.nilable(String))}
   def public_css_filename
-    File.basename(Dir.glob("public/index.*.css").first)
+    filepath = Dir.glob("public/index.*.css").first
+    return nil unless filepath
+
+    File.basename(filepath)
   end
 
   sig {params(markdown: String).returns(String)}
