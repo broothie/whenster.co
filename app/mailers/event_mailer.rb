57c269a2ac4b2140089b6d3249301359bef42ad9
broothie <adbooth8@gmail.com>
2023-02-14 23:57:07 -0800

    set up reminders

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -1,11 +1,31 @@
 class EventMailer < ApplicationMailer
-  def starting
+  def soon
     @event = Event.find(params[:event_id])
     @user = User.find(params[:user_id])
 
     mail(
       to: email_address_with_name(@user.email, @user.username),
-      subject: "#{@event.title} starts soon",
+      subject: "#{@event.title} starts soon!",
+    )
+  end
+
+  def tomorrow
+    @event = Event.find(params[:event_id])
+    @user = User.find(params[:user_id])
+
+    mail(
+      to: email_address_with_name(@user.email, @user.username),
+      subject: "#{@event.title} is tomorrow!",
+    )
+  end
+
+  def next_week
+    @event = Event.find(params[:event_id])
+    @user = User.find(params[:user_id])
+
+    mail(
+      to: email_address_with_name(@user.email, @user.username),
+      subject: "#{@event.title} is coming up!",
     )
   end
 end
--- spec/mailers/previews/event_mailer_preview.rb
+++ spec/mailers/previews/event_mailer_preview.rb
@@ -1,9 +1,9 @@
 # Preview all emails at http://localhost:3000/rails/mailers/event_mailer
 class EventMailerPreview < ActionMailer::Preview
-  def starting
+  def soon
     event = Event.find_by(id: params[:event_id]) || Event.last || FactoryBot.create(:event)
     user = event.users.first
 
-    EventMailer.with(event:, user:).starting
+    EventMailer.with(event:, user:).soon
   end
 end
