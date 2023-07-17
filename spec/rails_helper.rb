fc718fae503d5ceabe24285b416de7472aa6fd0d
broothie <adbooth8@gmail.com>
2023-07-17 10:55:17 -0700

    set up sidekiq testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,9 @@ require 'rspec/rails'
 # Webmock
 require 'webmock/rspec'
 
+# Sidekiq
+require "sidekiq/testing"
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
